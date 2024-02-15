67d33784742ead365dcbabe88087c7758ef59ded
dhrumil <dhrumilshivam@gmail.com>
2024-02-15 23:01:31 +0530

    django blogpost webapp

---
+++ blog/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from .models import Post #.models means from the currnet directory
+ 
+def home(request):
+    context = {
+        'posts': Post.objects.all()
+    }
+    return render(request, 'blog/home.html', context)
+
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'About'})
